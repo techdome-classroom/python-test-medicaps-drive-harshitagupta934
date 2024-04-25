0f6c9c9f286fd1814db9be7cb7e93f0773a58e38
Harshita gupta <84032318+harshitagupta934@users.noreply.github.com>
2024-04-25 13:59:29 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -4,4 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        
+        prev_value = 0
+        for char in reversed(s):
+            curr_value = roman_dict[char]
+            if curr_value < prev_value:
+                result -= curr_value
+            else:
+                result += curr_value
+            prev_value = curr_value
+        
+        return result
+
+# Test cases
+solution = Solution()
+print(solution.romanToInt("III"))     
+print(solution.romanToInt("LVIII"))   
+print(solution.romanToInt("MCMXCIV")) 
